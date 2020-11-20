21c2ad157e0a3bd7372a12004cd0dee92d4f8022
Joe Rickerby <joerick@mac.com>
2020-11-20 16:16:02 +0000

    Fix import syntax

--- dist/index.js
+++ dist/index.js
@@ -7,20 +7,36 @@ require('./sourcemap-register.js');module.exports =
 
 "use strict";
 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 var _a, _b;
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__webpack_require__(186));
-const github_1 = __importDefault(__webpack_require__(438));
+const core = __importStar(__webpack_require__(186));
+const github = __importStar(__webpack_require__(438));
 try {
-    const labels = (_b = (_a = github_1.default.context.payload) === null || _a === void 0 ? void 0 : _a.pull_request) === null || _b === void 0 ? void 0 : _b.labels;
-    core_1.default.debug(`PR labels: ${JSON.stringify(labels)}`);
-    core_1.default.setOutput('labels', labels);
+    const labels = (_b = (_a = github.context.payload) === null || _a === void 0 ? void 0 : _a.pull_request) === null || _b === void 0 ? void 0 : _b.labels;
+    core.debug(`PR labels: ${JSON.stringify(labels)}`);
+    core.setOutput('labels', labels);
 }
 catch (error) {
-    core_1.default.setFailed(error.message);
+    core.setFailed(error.message);
 }
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import core from '@actions/core';
-import github from '@actions/github';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 try {
     const labels = github.context.payload?.pull_request?.labels
